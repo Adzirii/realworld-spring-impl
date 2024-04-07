ee2835adde5f60d2d4f36cc84505e61d67c2a601
adziri <liubov.nikita@gmail.com>
2024-04-07 23:56:16 +0200

    spring init

---
+++ conduit-back/src/main/java/realworld/conduitback/ConduitBackApplication.java
@@ -0,0 +1,13 @@
+package realworld.conduitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConduitBackApplication.class, args);
+    }
+
+}
---
+++ conduit-back/src/test/java/realworld/conduitback/ConduitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package realworld.conduitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
